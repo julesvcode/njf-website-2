28f541e3c32db79b65539a37168c162c26e954a6
Julien V <vier.julien@googlemail.com>
2022-09-19 11:07:41 +0200

    Mobile navbar

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+// NAVBAR BURGER MENU
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("nav__menu")[0];
+
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+})
+
+
+// FAQ DROPDOWNS
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
@@ -5,3 +16,4 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     });
 });
+
