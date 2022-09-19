3f0e5842e08ff9c51c34b0b22d3d5ab5d048ce26
Julien V <vier.julien@googlemail.com>
2022-09-19 18:44:02 +0200

    Updates before live

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-// NAVBAR BURGER MENU
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("nav__menu")[0];
+// // NAVBAR BURGER MENU
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navbarLinks = document.getElementsByClassName("nav__menu")[0];
 
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-})
+// toggleButton.addEventListener("click", () => {
+//     navbarLinks.classList.toggle("active");
+// })
 
 
 // FAQ DROPDOWNS
